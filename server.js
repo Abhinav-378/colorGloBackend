0ae3420188a8a0a4d6b64e5f51690809309cb490
Abhinav Choukse <124260348+Abhinav-378@users.noreply.github.com>
2024-08-28 21:39:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+require('dotenv').config();
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Create a transporter using Gmail SMTP
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+app.post('/api/send', (req, res) => {  // Updated route
+    const { name, email, mobile, message } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: 'u22cs086@coed.svnit.ac.in',
+        subject: 'Contact Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error:', error); 
+            res.status(500).send({ message: 'Failed to send email' });
+        } else {
+            console.log('Email sent:', info.response);
+            res.status(200).send({ message: 'Email sent successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
